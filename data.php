0d4669d417cb1451011de2e30345af3151ddee05
NAHID SHAHAREYAR SETU <103564185+SETU-xyz@users.noreply.github.com>
2024-04-03 13:54:49 +0600

    Create data.php

---
+++ data.php
@@ -0,0 +1,1 @@
+{ "Provider": "DarkAlien69", "Server": 1, "status": 200, "Contract": "@DarkAlien_69", "msg": "Contract With admin to Get Authorized", "token": { "url": "https://khulnafashion.store/api.php", "apiKey": "ee58c3f374147735e27be3c2bd30e095" } }
